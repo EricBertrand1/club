1b4dcba944c4829872a5c4b5768ea798c783d243
eric.bertrand <eric.bertrand.france@gmail.com>
2025-09-17 15:31:12 +0200

    Ajout de Stimulus, mise à jour du fichier app.js et ajout des contrôleurs

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,15 @@
+// Importer bootstrap.js (si nécessaire pour ton projet)
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+
+// Importer Stimulus
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+
+// Démarrer l'application Stimulus
+const application = Application.start();
+
+// Charger les contrôleurs Stimulus depuis le répertoire 'controllers'
+const context = require.context('./controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
